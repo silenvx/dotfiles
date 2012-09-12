a122c211bcb7d99d8d2aa8c3156ba618799476a8
silenvx <czvziwzx@gmail.com>
2012-09-12 19:29:18 +0900

    add setsym.sh .setsymfunc.sh .setsymlist

---
+++ .setsymfunc.sh
@@ -0,0 +1,208 @@
+# 関数の種類{{{
+
+# 識別名 必要な引数の数 というように書きます
+SYM_FUNCLIST="default 2
+firefox 2"
+# }}}関数の種類
+# setsym.shに呼び出される関数{{{
+
+# ${SYM_FUNCLIST}に書いた識別名の処理を書きます
+# -iの時にはfunc_install_識別名
+# -rの時にはfunc_remove_識別名 が呼び出されます
+#
+# -fは${FLAG_f}でtrueもしくはfalseになっていて有効だとtrueになります
+# -fの処理や関数は各自で作成してください
+func_force_clean(){
+    if [ -d "${2}" -a -e "${2}/${1##*/}" ];then
+        echo rm -rf "${2}/${1##*/}"
+        rm -rf "${2}/${1##*/}"
+        return 0
+    else
+        if [ -f "${2}" ];then
+            echo rm -rf "${2}"
+            rm -rf "${2}"
+            return 0
+        else
+            ls -ad "${2}/${1##*/}" >/dev/null 2>&1
+            if [ "${?}" == "0" ];then
+                if [ ! -e `realpath -m "${2}/${1}"` ];then
+                    echo rm -rf "${2}/${1##*/}"
+                    rm -rf "${2}/${1##*/}"
+                    return 0
+                fi
+            fi
+            ls -ad "${2}" >/dev/null 2>&1
+            if [ "${?}" == "0" ];then
+                if [ ! -e `realpath -m "${2}"` ];then
+                    echo rm -rf "${2}"
+                    rm -rf "${2}"
+                    return 1
+                fi
+            fi
+        fi
+    fi
+}
+
+# default{{{
+
+# .setsymlistの default <source> <dest>が
+# ln -s <source> <dest>の処理をします
+# 外部からこの関数を使うなら
+# func_install_default <source> <dest>と大体同じです
+# -fで既にファイルがあった場合それを消してln -sをする
+func_install_default(){
+    LN_SOURCE="$(eval eval realpath -ms ${1})"
+    LN_DEST="$(eval eval realpath -ms ${2})"
+    if [ "${FLAG_f}" == "true" ];then
+        func_force_clean "${LN_SOURCE}" "${LN_DEST}"
+    fi
+    if [ -e "${LN_SOURCE}" ];then
+        if [ -d "${LN_DEST}" -a -e "${LN_DEST}/${LN_SOURCE##*/}" ];then
+            echo "a既に${LN_DEST}/${LN_SOURCE##*/}がありました" >&2
+            return 1
+        else
+            if [ -f "${LN_DEST}" ];then
+                echo "既に${LN_DEST}がありました" >&2
+                return 1
+            else
+                if [ "${FLAG_f}" == "true" -a ! -d "${LN_DEST%/*}" ];then
+                    echo mkdir -p "${LN_DEST%/*}"
+                    mkdir -p "${LN_DEST%/*}"
+                fi
+                ls -ad "${LN_DEST}/${LN_SOURCE##*/}" >/dev/null 2>&1
+                if [ "${?}" == "0" ];then
+                    if [ ! -e `realpath -m "${LN_DEST}/${LN_SOURCE}"` ];then
+                        echo "既に${LN_DEST}/${LN_SOURCE##*/}がありましたがシンボリックの先がない可能性があります" >&2
+                        return 1
+                    fi
+                fi
+                ls -ad "${LN_DEST}" >/dev/null 2>&1
+                if [ "${?}" == "0" ];then
+                    if [ ! -e `realpath -m "${LN_DEST}"` ];then
+                        echo "既に${LN_DEST}がありましたがシンボリックの先がない可能性があります" >&2
+                        return 1
+                    fi
+                fi
+                echo ln -s "${LN_SOURCE}" "${LN_DEST}"
+                ln -s "${LN_SOURCE}" "${LN_DEST}"
+            fi
+        fi
+    else
+        echo "${LN_SOURCE}がありません" >&2
+        return 1
+    fi
+}
+func_remove_default(){
+    LN_SOURCE="$(eval eval realpath -ms ${1})"
+    LN_DEST="$(eval eval realpath -ms ${2})"
+    if [ "${FLAG_f}" == "true" ];then
+        func_force_clean "${LN_SOURCE}" "${LN_DEST}"
+        return 0
+    fi
+    if [ -e "${LN_SOURCE}" ];then
+        if [ -d "${LN_DEST}" -a -e "${LN_DEST}/${LN_SOURCE##*/}" ];then
+            if [ "${LN_SOURCE}" == `realpath -m "${LN_DEST}/${LN_SOURCE##*/}"` ];then
+                echo rm "${LN_DEST}/${LN_SOURCE##*/}"
+                rm "${LN_DEST}/${LN_SOURCE##*/}"
+                return 0
+            else
+                echo "${LN_DEST}/${LN_SOURCE##*/}は現在のスクリプトなどで貼ったものではありません" >&2
+            fi
+        else
+            if [ -f "${LN_DEST}" ];then
+                if [ "${LN_SOURCE}" == `realpath -m "${LN_DEST}"` ];then
+                    echo rm "${LN_DEST}"
+                    rm "${LN_DEST}"
+                    return 0
+                else
+                    echo "${LN_DEST}は現在のスクリプトなどで貼ったものではありません" >&2
+                fi
+            else
+                ls -ad "${LN_DEST}/${LN_SOURCE##*/}" >/dev/null 2>&1
+                if [ "${?}" == "0" ];then
+                    if [ ! -e `realpath -m "${LN_DEST}/${LN_SOURCE}"` ];then
+                        echo "${LN_DEST}は現在のスクリプトなどで貼ったものではありませんがシンボリックの先がない可能性があります" >&2
+                        return 1
+                    fi
+                fi
+                ls -ad "${LN_DEST}" >/dev/null 2>&1
+                if [ "${?}" == "0" ];then
+                    if [ ! -e `realpath -m "${LN_DEST}"` ];then
+                        echo "${LN_DEST}は現在のスクリプトなどで貼ったものではありませんがシンボリックの先がない可能性があります" >&2
+                        return 1
+                    fi
+                fi
+            fi
+        fi
+    else
+        echo "${LN_SOURCE}がありません" >&2
+        return 1
+    fi
+}
+# }}}default
+# firefox{{{
+
+# .setsymlistの firefox <source> <dest>
+# ln -s <source> <firefoxのprofile>/<dest>の処理をします
+# firefoxのprofileは${FIREFOX_PROFILES_INI}の変数のファイルを見て決めます
+# 複数プロファイルがあった場合はメニューを表示して入力待ちをします
+# func_firefox_profileがそういうプロファイルの場所を決める関数です
+FIREFOX_PROFILES_INI=`realpath ~/.mozilla/firefox/profiles.ini`
+func_firefox_profile(){
+    if [ -f "${FIREFOX_PROFILES_INI}" ];then
+        i=0
+        IFS=$'\n'
+        for TMP in `grep "^Path=" "${FIREFOX_PROFILES_INI}"`;do
+            i=`expr ${i} + 1`
+            eval FIREFOX_PROFILE_PATH${i}=`cd ${FIREFOX_PROFILES_INI%/*};realpath "${TMP#Path=}"`
+        done
+        if [ "${i}" == "0" ];then
+            echo "${FIREFOX_PROFILES_INI}にプロファイルディレクトリの記載がありませんでした" >&2
+            return 1
+        elif [ "${i}" == "1" ];then
+            k="1"
+        elif [ "${i}" -ge "2" ];then
+            echo "番号でfirefoxのprofileのパスを指定してください" >&2
+            for j in `seq 1 ${i}`;do
+                eval echo \"${j}:\${FIREFOX_PROFILE_PATH${j}}\" >&2
+            done
+            echo -n " > " >&2
+            read k
+            while [ ! \( "${k}" -ge "1" -a "${k}" -le "${i}" \) ];do
+                echo -n "1から${i}の間を入力してください > " >&2
+                read k
+            done
+        else
+            echo "予期せぬエラーが発生しました" >&2
+            return 1
+        fi
+        FIREFOX_PROFILE_PATH=$(eval echo "\${FIREFOX_PROFILE_PATH${k}}")
+        if [ ! -d "${FIREFOX_PROFILE_PATH}" ];then
+            echo "firefoxのプロファイルディレクトリ${FIREFOX_PROFILE_PATH}がないです" >&2
+            return 1
+        fi
+        echo "${FIREFOX_PROFILE_PATH}"
+        return 0
+    else
+        echo "${FIREFOX_PROFILES_INI}が見つかりませんでした" >&2
+        return 1
+    fi
+}
+func_install_firefox(){
+    FIREFOX_PROFILE_PATH=`func_firefox_profile`
+    if [ "${?}" == "1" ];then
+        return 1
+    fi
+    func_install_default "${1}" "${FIREFOX_PROFILE_PATH}/${2}"
+}
+func_remove_firefox(){
+    FIREFOX_PROFILE_PATH=`func_firefox_profile`
+    if [ "${?}" == "1" ];then
+        return 1
+    fi
+    func_remove_default "${1}" "${FIREFOX_PROFILE_PATH}/${2}"
+}
+# }}}firefox
+# }}}setsym.shに呼び出される関数
+
+# vim: set fdm=marker:
---
+++ setsym.sh
@@ -0,0 +1,128 @@
+#!/bin/sh
+VERSION="0.0.1"
+PWD="$(cd $(dirname ${0});pwd)"
+FLAG_i="false"
+FLAG_r="false"
+FLAG_f="false"
+SYM_FUNCARG="0"
+# このファイルを見ながらファイルを設置する
+SYM_FILELIST="./.setsymlist"
+# 関数をまとめたファイル
+source "./.setsymfunc.sh"
+
+# ヘルプ関数{{{
+func_help(){
+    echo "${0##*/} version:${VERSION}"
+    echo "このコマンドの使い方"
+    echo "-i        シンボリックリンクを設置"
+    echo "-r        シンボリックリンクを削除"
+    echo "-if       強制的に設置 (既にファイルがある場合は消す、ディレクトリがない場合は作ります)"
+    echo "-rf       強制的に削除 (設置してなさそうなシンボリックリンクでも消す)"
+    echo "-l <file> シンボリックリンクを貼るリストが書いてあるファイルを指定 (通常は./.symfilelist)"
+    echo "-h        このヘルプを表示"
+    echo "----------"
+    echo "例: ${0##*/} -if -l ${SYM_FILELIST}"
+    exit 0
+}
+# }}}ヘルプ関数
+# オプション判定{{{
+if [ "${#}" -lt "1" ];then
+    func_help >&2
+fi
+
+unset GETOPT_COMPATIBLE
+OPT=`getopt -o irfl:h -- "${@}"`
+if [ "${?}" != 0 ];then
+    func_help >&2
+    exit 1
+fi
+
+eval set -- "${OPT}"
+
+for i;do
+    case ${i} in
+        -i) FLAG_i="true";;
+        -r) FLAG_r="true";;
+        -f) FLAG_f="true";;
+        -l) SYM_FILELIST="${2}"
+            shift;;
+        -h) func_help;;
+        --) break;;
+    esac
+    shift
+done
+# }}}オプション判定
+# オプションのエラーチェック{{{
+if [ "${FLAG_i}" == "false" -a "${FLAG_r}" == "false" ];then
+    echo "-iまたは-rのどちらかを有効にしてください" >&2
+    exit 1
+elif [ "${FLAG_i}" == "true" -a "${FLAG_r}" == "true" ];then
+    echo "-iまたは-rのどちらかを無効にしてください" >&2
+    exit 1
+fi
+
+if [ ! -e "${SYM_FILELIST}" ];then
+    echo "${SYM_FILELIST}が存在しません" >&2
+    exit 1
+fi
+#}}}オプションのエラーチェック
+# ${SYM_FILELIST}の構文チェック{{{
+IFS=$'\n'
+for i in `cat "${SYM_FILELIST}"`;do
+    echo "${i}"|grep -v "^#" >/dev/null 2>&1
+    if [ "${?}" == 0 ];then
+        IFS=$'\n'
+        for n in ${SYM_FUNCLIST};do
+            if [ "${i%% *}" == "${n%% *}" ];then
+                SYM_FUNCARG="${n##* }"
+            fi
+        done
+        if [ "${SYM_FUNCARG}" == "0" ];then
+            echo "${SYM_FILELIST}の`grep -n \"^${i}$\" \"${SYM_FILELIST}\"`に誤りがありました" >&2
+            echo "${i%% *}の処理は設定されていません" >&2
+            exit 1
+        fi
+        k="0"
+        IFS=" "
+        for j in ${i};do
+            if [ "${k}" -gt "${SYM_FUNCARG}" ];then
+                echo "${SYM_FILELIST}の`grep -n \"^${i}$\" \"${SYM_FILELIST}\"`に誤りがありました" >&2
+                echo "値の数が多いです" >&2
+                exit 1
+            fi
+            echo "${j}"|grep "^'.*'$" >/dev/null 2>&1
+            k=`expr ${k} + 1`
+        done
+        if [ "${k}" == "${SYM_FUNCARG}" ];then
+            echo "${SYM_FILELIST}の`grep -n \"^${i}$\" \"${SYM_FILELIST}\"`に誤りがありました" >&2
+            echo "値の数が少ないです" >&2
+            exit 1
+        fi
+        if [ -z "${SYM_LIST}" ];then
+            SYM_LIST="${i}"
+        else
+            SYM_LIST="${SYM_LIST}"$'\n'"${i}"
+        fi
+    fi
+    IFS=$'\n'
+done
+# }}}${SYM_FILELIST}の構文チェック
+# 処理実行{{{
+if [ "${FLAG_i}" == "true" -o "${FLAG_r}" == "true" ];then
+    if [ "${FLAG_i}" == "true" -a "${FLAG_r}" == "false" ];then
+        FLAG_mode="install"
+    elif [ "${FLAG_i}" == "false" -a "${FLAG_r}" == "true" ];then
+        FLAG_mode="remove"
+    else
+        exit 1
+    fi
+    IFS=$'\n'
+    for i in ${SYM_LIST};do
+        IFS=" "
+        func_${FLAG_mode}_${i%% *} ${i#* }
+        IFS=$'\n'
+    done
+fi
+# }}}処理実行
+
+# vim: set fdm=marker:
